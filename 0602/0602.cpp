538ba89283360a1c2674ed51d0e97284d12edbb5
4D-12\koyo <a>
2026-06-02 15:44:27 +0900

    ランダムを追加

--- 0602/0602.cpp
+++ 0602/0602.cpp
@@ -1,21 +1,29 @@
 ﻿#include <iostream>
 #include "EnemyFactory.h"
 #include "Enemy.h"
+#include <random>
 
 int main()
 {
     //　HPの合計を格納する変数
     int HP_Sum = 0;
+    //　ATKの合計を格納する変数
     int ATK_Avg = 0;
 
     for (int i = 1; i <= 4; i++)
     {
-        Enemy* enemy = EnemyFactory::CreateEnemy(i);
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution<int> dist(1, 4);
+
+        int random = dist(mt);
+
+        Enemy* enemy = EnemyFactory::CreateEnemy(random);
 
         std::cout << enemy->Data.Name << " EXP:" << enemy->Data.EXP << ", Gold:" << enemy->Data.Gold << ", Critical Rate:" << enemy->Data.CriticalRate << std::endl;
 
-		HP_Sum += enemy->Data.HP;
-		ATK_Avg += enemy->Data.ATK;
+        HP_Sum += enemy->Data.HP;
+        ATK_Avg += enemy->Data.ATK;
 
         delete enemy;
     }
