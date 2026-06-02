b66d5b9d5c4fd21cb61f3a8e206774319e1162d6
4D-12\koyo <a>
2026-06-02 13:56:47 +0900

    プログラム完成

--- 0602/Enemy.h
+++ 0602/Enemy.h
@@ -1,13 +1,13 @@
 #pragma once
-#include "Element.h"
+#include "EnemyData.h"
 
 //　@brief 敵のデータを表す構造体
 //　@details
 //　この構造体は、敵の基本的な属性を格納します
-struct EnemyData
+struct Enemy
 {
 	int ID;
-	const char Name[256];
+	const char* Name;
 	int HP;
 	int ATK;
 	int DEF;
@@ -15,4 +15,17 @@ struct EnemyData
 	int Gold;
 	int EXP;
 	Element Element;
+
+	Enemy(const EnemyData& data)
+		: ID(data.ID),
+		Name(data.Name),
+		HP(data.HP),
+		ATK(data.ATK),
+		DEF(data.DEF),
+		SPD(data.SPD),
+		Gold(data.Gold),
+		EXP(data.EXP),
+		Element(data.Element)
+	{
+	}
 };
